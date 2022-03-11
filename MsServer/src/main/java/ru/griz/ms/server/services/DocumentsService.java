6387dba5446e8b3b8f669c4981b7372e8d60a8ad
griz <grizlen@rambler.ru>
2022-03-11 12:34:46 +0500

    server optimize

--- MsServer/src/main/java/ru/griz/ms/server/services/DocumentsService.java
+++ MsServer/src/main/java/ru/griz/ms/server/services/DocumentsService.java
@@ -2,18 +2,78 @@ package ru.griz.ms.server.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.griz.ms.server.entities.Document;
-import ru.griz.ms.server.repositories.DocumentsRepository;
+import ru.griz.ms.server.entities.*;
+import ru.griz.ms.server.exceptions.ResourceNotFoundException;
+import ru.griz.ms.server.repositories.*;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 @Service
 @RequiredArgsConstructor
 public class DocumentsService {
 
     private final DocumentsRepository documentsRepository;
+    private final BuyRepository buyRepository;
+    private final BuyItemRepository buyItemRepository;
+    private final SaleRepository saleRepository;
+    private final SaleItemRepository saleItemRepository;
+    private final ReturnRepository returnRepository;
+    private final ReturnItemRepository returnItemRepository;
 
-    public List<Document> getAll() {
+    private Supplier<ResourceNotFoundException> documentNotFound(Long id) {
+        return () -> new ResourceNotFoundException("Document: " + id + " not found");
+    }
+
+    // Все документы
+    public List<Document> getAllDocs() {
         return documentsRepository.findAll();
+    }
+
+    public Document getByIdDoc(Long id) {
+        return documentsRepository.findById(id)
+                .orElseThrow(documentNotFound(id));
+    }
+
+    // Поступления
+    public List<BuyHeader> getAllDocBuys() {
+        return buyRepository.findAll();
+    }
+
+    public BuyHeader getByIdDocBuy(Long id) {
+        return buyRepository.findById(id)
+                .orElseThrow(documentNotFound(id));
+    }
+
+    public List<BuyItem> getDocBuyItems(long docId) {
+        return buyItemRepository.findAllByDocId(docId);
+    }
+
+    // Отгрузки
+    public List<SaleHeader> getAllDocSales() {
+        return saleRepository.findAll();
+    }
+
+    public SaleHeader getByIdDocSale(Long id) {
+        return saleRepository.findById(id)
+                .orElseThrow(documentNotFound(id));
+    }
+
+    public List<SaleItem> getDocSaleItems(long docId) {
+        return saleItemRepository.findAllByDocId(docId);
+    }
+
+    // Возвраты
+    public List<ReturnHeader> getAllDocReturns() {
+        return returnRepository.findAll();
+    }
+
+    public ReturnHeader getByIdDocReturn(Long id) {
+        return returnRepository.findById(id)
+                .orElseThrow(documentNotFound(id));
+    }
+
+    public List<ReturnItem> getDocReturnItems(long docId) {
+        return returnItemRepository.findAllByDocId(docId);
     }
 }
